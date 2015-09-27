153c84e6a19b253701993efadc0c913eebdd4302
Andre Steenveld <andre.steenveld@gmail.com>
2015-09-27 14:24:33 +0200

    The application runs, and works!

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
-//Note: This file is provided as an aid to help you get up and running with
-//Electron for desktop apps. See the readme file for more information.
+//
+// This will be the main entry point before anything actually happens. systemjs
+// has been loaded but we might would like to configure some stuff before we kick
+// off the application. In this case we want to add a catch-all path which used
+// to be in the config.js file from yo:aurelia.
+//
+!( "*" in System.paths ) && ( System.paths[ "*" ] = "application/*" );
 
-'use strict';
-
-var app = require('app');
-var BrowserWindow = require('browser-window');
-
-require('crash-reporter').start();
-
-var mainWindow = null;
-
-app.on('window-all-closed', function () {
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('ready', function () {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600
-    });
-
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
-    mainWindow.webContents.on('did-finish-load', function () {
-        mainWindow.setTitle(app.getName());
-    });
-    mainWindow.on('closed', function () {
-        mainWindow = null;
-    });
-});
+// Now kick off the entire application
+System.import( "aurelia-bootstrapper" );
