3423c801b8326ec430af5e69c9797274b7146e10
Andre Steenveld <andre.steenveld@gmail.com>
2015-09-27 13:48:14 +0200

    Changing the paths so we output in to the correct directory

--- build/paths.js
+++ build/paths.js
@@ -1,16 +1,16 @@
 var path = require('path');
 
-var appRoot = 'src/';
-var outputRoot = 'dist/';
+var appRoot = 'src/application';
+var outputRoot = 'cordova/www';
 
 module.exports = {
-  root: appRoot,
-  source: appRoot + '**/*.js',
-  html: appRoot + '**/*.html',
-  css: appRoot + '**/*.css',
-  style: 'styles/**/*.css',
-  output: outputRoot,
-  doc:'./doc',
-  e2eSpecsSrc: 'test/e2e/src/*.js',
-  e2eSpecsDist: 'test/e2e/dist/'
+	root: appRoot,
+	source: appRoot + '**/*.js',
+	html: appRoot + '**/*.html',
+	css: appRoot + '**/*.css',
+	style: 'styles/**/*.css',
+	output: outputRoot,
+	doc:'./doc',
+	e2eSpecsSrc: 'test/e2e/src/*.js',
+	e2eSpecsDist: 'test/e2e/dist/'
 };
